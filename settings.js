995150d6297187fb60274570082a23a317719ee1
cooling23 <137326420+cooling23@users.noreply.github.com>
2024-08-19 07:12:41 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,13 +10,13 @@ global.ytname = "David Cyril"
 global.socialm = "GitHub: DeeCeeXxx"
 global.location = "Africa, Nigeria, Lagos"
 
-global.ownernumber = '2347043759577,2349066528353,254782572110'  //creator number
-global.ownername = 'David Cyril' //owner name
-global.botname = '🕷Venom-2🕷' //name of the bot
+global.ownernumber = '2347043759577,2349066528353,01008533039'  //creator number
+global.ownername = 'booooot' //owner name
+global.botname = 'bot mo' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'David_Cyril\n\nContact: +2349066528353'
+global.author = 'mo'
 
 //console view/theme
 global.themeemoji = '🪀'
